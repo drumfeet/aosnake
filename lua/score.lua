2c7ed87320f32d91fcb1d2a4a956ce334bbc9f1b
drumfeet <drumfeet@gmail.com>
2024-07-24 16:57:44 +0200

    update lua script

--- lua/score.lua
+++ lua/score.lua
@@ -5,17 +5,25 @@ Handlers.add(
     Handlers.utils.hasMatchingTag("Action", "Update"),
     function(msg)
         assert(type(msg.score) == 'string', 'score is required!')
-        HighScore = msg.score
-        Handlers.utils.reply("HighScore updated to " .. msg.score)(msg)
+
+        local newScore = tonumber(msg.score)
+        assert(newScore, 'score must be a valid number!')
+
+        if newScore > HighScore then
+            HighScore = newScore
+            Handlers.utils.reply("HighScore updated to " .. newScore)(msg)
+        else
+            Handlers.utils.reply("Submitted score is not higher than the current HighScore.")(msg)
+        end
     end
 )
 
 Handlers.add(
     "GetHighScore",
     Handlers.utils.hasMatchingTag("Action", "GetHighScore"),
-    function(Msg)
+    function(msg)
         ao.send({
-            Target = Msg.From,
+            Target = msg.From,
             HighScore = HighScore
         })
     end
